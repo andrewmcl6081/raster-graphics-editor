7bfb85e505db92022e19ded270334ed10ea201fc
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-06-18 00:27:20 -0500

    started working on crop functionality

--- program1.cpp
+++ program1.cpp
@@ -10,7 +10,11 @@ const std::string windowName = "Raster Graphics";
 int toolIndex = -1;
 
 const std::vector<std::string> tools = {"Eyedropper", "Crop", "Pencil", "Paint Bucket", "Reset"};
+
 std::vector<size_t> bgr_val = {255,255,255};
+
+// Cropping Dependencies
+std::vector<std::vector<int>> cropped_points;
 
 std::string print_bgr() {
 	std::string bgr_str = 
@@ -37,6 +41,7 @@ void eyeDropper(int event, int x, int y, int flags, void* param) {
 
 void cycleToolsOn_RClick(int event, int x, int y, int flags, void* param) {
 
+	cv::Mat* imageIn = (cv::Mat*)param;
 
 	if(event == cv::EVENT_RBUTTONDOWN) {
 		// on event, select first tool and ensure we cycle through tools
@@ -50,7 +55,50 @@ void cycleToolsOn_RClick(int event, int x, int y, int flags, void* param) {
 	if (toolIndex == 0 && event == cv::EVENT_LBUTTONDOWN) {
 		eyeDropper(event, x, y, flags, param);
 	}
-	elseif (toolIndex == 1 && event == )
+	else if (toolIndex == 1) {
+		
+		if (event == cv::EVENT_LBUTTONDOWN) {
+			std::cout << "inside lbd" << std::endl;
+
+			std::vector<int> point;
+
+			point.push_back(x);
+			point.push_back(y);
+
+			cropped_points.push_back(point);
+		}
+		else if(event == cv::EVENT_LBUTTONUP) {
+			std::cout << "inside lbu" << std::endl;
+			
+			std::vector<int> point;
+
+			point.push_back(x);
+			point.push_back(y);
+
+			cropped_points.push_back(point);
+
+			cv::Point p1(cropped_points[0][0], cropped_points[0][1]);
+			cv::Point p2(cropped_points[1][0], cropped_points[1][1]);
+			cv::Rect region(p1,p2);
+			
+			cv::Mat imageROI = imageIn(region);
+
+			// cv::resize(imageIn, imageROI, imageROI.size());
+			// *imageIn = imageROI.clone();
+			// imageIn = imageIn(region);
+			
+			// cv::Mat imageResized;
+			// cv::resize(imageIn, )
+
+			//cv::imshow(windowName, imageIn);
+
+			// std::cout << "image width: " << imageIn.size().width << std::endl;
+			// std::cout << "image height: " << imageIn.size().height << std::endl;
+			// std::cout << "image channels: " << imageIn.channels() << std::endl;
+		}
+	}
+
+
 }
 
 int main(int argc, char **argv)
@@ -77,9 +125,17 @@ int main(int argc, char **argv)
 		{
 			cv::imshow(windowName, imageIn);
 
+			std::cout << "image width: " << imageIn.size().width << std::endl;
+			std::cout << "image height: " << imageIn.size().height << std::endl;
+			std::cout << "image channels: " << imageIn.channels() << std::endl;
+
 			std::cout << "Right click to cycle through available tools" << std::endl;
 			cv::setMouseCallback(windowName, cycleToolsOn_RClick, &imageIn);
 			cv::waitKey();
+
+			std::cout << cropped_points[0][0] << " " << cropped_points[0][1] << std::endl;
+			std::cout << cropped_points[1][0] << " " << cropped_points[1][1] << std::endl;
+			std::cout << cropped_points.size() << std::endl;
 		}
     }
 }
