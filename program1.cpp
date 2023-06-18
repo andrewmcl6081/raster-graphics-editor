0aaccbc8cad53f724412d6d929f2d8e8307e388d
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-06-18 14:03:29 -0500

    fixed crop to work properly

--- program1.cpp
+++ program1.cpp
@@ -4,102 +4,27 @@
 //#include "test.h"
 #include "opencv2/opencv.hpp"
 
+struct dataStruct {
+	cv::Mat* image;
+	bool needsUpdate;
+};
+
+
+void eyeDropper(int event, int x, int y, int flags, void* param);
+void toolsEvent(int event, int x, int y, int flags, void* param);
+std::string print_bgr();
+
 const std::string windowName = "Raster Graphics";
 
 // No tool should be selected by default, this index maps to the tools vector
 int toolIndex = -1;
 
-const std::vector<std::string> tools = {"Eyedropper", "Crop", "Pencil", "Paint Bucket", "Reset"};
+// Cropping Dependencies
+std::vector<int> point;
 
+const std::vector<std::string> tools = {"Eyedropper", "Crop", "Pencil", "Paint Bucket", "Reset"};
 std::vector<size_t> bgr_val = {255,255,255};
 
-// Cropping Dependencies
-std::vector<std::vector<int>> cropped_points;
-
-std::string print_bgr() {
-	std::string bgr_str = 
-		"(B: " + std::to_string(bgr_val[0]) + 
-		", " + 
-		"G: " + std::to_string(bgr_val[1]) + 
-		", " + 
-		"R: " + std::to_string(bgr_val[2]) + 
-		")";
-	
-	return bgr_str;
-}
-
-void eyeDropper(int event, int x, int y, int flags, void* param) {
-
-	cv::Mat imageIn = *(cv::Mat*)param;
-
-	bgr_val[0] = imageIn.at<cv::Vec3b>(y,x)[0];
-	bgr_val[1] = imageIn.at<cv::Vec3b>(y,x)[1];
-	bgr_val[2] = imageIn.at<cv::Vec3b>(y,x)[2];
-
-	std::cout << print_bgr() << std::endl;
-}
-
-void cycleToolsOn_RClick(int event, int x, int y, int flags, void* param) {
-
-	cv::Mat* imageIn = (cv::Mat*)param;
-
-	if(event == cv::EVENT_RBUTTONDOWN) {
-		// on event, select first tool and ensure we cycle through tools
-		// multiple times
-		toolIndex = (toolIndex + 1) % 5;
-
-		std::cout << "Selected tool : " << tools[toolIndex] << std::endl;
-	}
-
-
-	if (toolIndex == 0 && event == cv::EVENT_LBUTTONDOWN) {
-		eyeDropper(event, x, y, flags, param);
-	}
-	else if (toolIndex == 1) {
-		
-		if (event == cv::EVENT_LBUTTONDOWN) {
-			std::cout << "inside lbd" << std::endl;
-
-			std::vector<int> point;
-
-			point.push_back(x);
-			point.push_back(y);
-
-			cropped_points.push_back(point);
-		}
-		else if(event == cv::EVENT_LBUTTONUP) {
-			std::cout << "inside lbu" << std::endl;
-			
-			std::vector<int> point;
-
-			point.push_back(x);
-			point.push_back(y);
-
-			cropped_points.push_back(point);
-
-			cv::Point p1(cropped_points[0][0], cropped_points[0][1]);
-			cv::Point p2(cropped_points[1][0], cropped_points[1][1]);
-			cv::Rect region(p1,p2);
-			
-			cv::Mat imageROI = imageIn(region);
-
-			// cv::resize(imageIn, imageROI, imageROI.size());
-			// *imageIn = imageROI.clone();
-			// imageIn = imageIn(region);
-			
-			// cv::Mat imageResized;
-			// cv::resize(imageIn, )
-
-			//cv::imshow(windowName, imageIn);
-
-			// std::cout << "image width: " << imageIn.size().width << std::endl;
-			// std::cout << "image height: " << imageIn.size().height << std::endl;
-			// std::cout << "image channels: " << imageIn.channels() << std::endl;
-		}
-	}
-
-
-}
 
 int main(int argc, char **argv)
 {
@@ -121,21 +46,102 @@ int main(int argc, char **argv)
 			std::cout << "Error while opening file " << argv[1] << std::endl;
 			return 0;
 		}
-		else
-		{
-			cv::imshow(windowName, imageIn);
 
-			std::cout << "image width: " << imageIn.size().width << std::endl;
-			std::cout << "image height: " << imageIn.size().height << std::endl;
-			std::cout << "image channels: " << imageIn.channels() << std::endl;
+		// Initialize our struct to manage image data
+		dataStruct imgData;
+		imgData.image = &imageIn;
+		imgData.needsUpdate = false;
 
-			std::cout << "Right click to cycle through available tools" << std::endl;
-			cv::setMouseCallback(windowName, cycleToolsOn_RClick, &imageIn);
-			cv::waitKey();
 
-			std::cout << cropped_points[0][0] << " " << cropped_points[0][1] << std::endl;
-			std::cout << cropped_points[1][0] << " " << cropped_points[1][1] << std::endl;
-			std::cout << cropped_points.size() << std::endl;
-		}
+		cv::imshow(windowName, imageIn);
+		cv::setMouseCallback(windowName, toolsEvent, &imgData);
+		cv::waitKey();
+
     }
+}
+
+
+void toolsEvent(int event, int x, int y, int flags, void* param) {
+
+	std::cout << "in func" << std::endl;
+
+	// Pointer to the struct created in main
+	dataStruct* imgData = static_cast<dataStruct*>(param);
+
+	// pointer to the original imageIn
+	cv::Mat* image_ptr = imgData->image;
+
+
+	// Check if we need to update the image from cropping
+	// if(imgData->needsUpdate == true) {
+	// 	std::cout << "we need to update the window" << std::endl;
+	// 	cv::imshow(windowName, *image);
+	// 	imgData->needsUpdate = false;
+	// }
+
+	if(event == cv::EVENT_RBUTTONDOWN) {
+		// on event, select first tool and ensure we cycle through tools
+		// multiple times
+		toolIndex = (toolIndex + 1) % 5;
+
+		std::cout << "Selected tool : " << tools[toolIndex] << std::endl;
+	}
+	else if (toolIndex == 0 && event == cv::EVENT_LBUTTONDOWN) {
+
+		bgr_val[0] = image_ptr->at<cv::Vec3b>(y,x)[0];
+		bgr_val[1] = image_ptr->at<cv::Vec3b>(y,x)[1];
+		bgr_val[2] = image_ptr->at<cv::Vec3b>(y,x)[2];
+
+		std::cout << print_bgr() << std::endl;
+	}
+	else if (toolIndex == 1) {
+		
+		if (event == cv::EVENT_LBUTTONDOWN) {
+
+			point.push_back(x);
+			point.push_back(y);
+		}
+		else if (event == cv::EVENT_LBUTTONUP) {
+
+			cv::Point p1(point[0], point[1]);
+			cv::Point p2(x,y);
+			cv::Rect region(p1,p2);
+
+			//std::cout << "p1 : " << p1 << " " << "p2 : " << p2 << std::endl;
+			
+			
+			// Grabbing ROI from original imageIn and updating imageIn with new ROI image
+			cv::Mat imageROI = (*image_ptr)(region);
+			*image_ptr = imageROI;
+			cv::imshow(windowName, imageROI);
+
+			point.clear();
+		}
+	}	
+
+}
+
+
+// void eyeDropper(int event, int x, int y, int flags, void* param) {
+
+// 	cv::Mat imageIn = *(cv::Mat*)param;
+
+// 	bgr_val[0] = imageIn.at<cv::Vec3b>(y,x)[0];
+// 	bgr_val[1] = imageIn.at<cv::Vec3b>(y,x)[1];
+// 	bgr_val[2] = imageIn.at<cv::Vec3b>(y,x)[2];
+
+// 	std::cout << print_bgr() << std::endl;
+// }
+
+
+std::string print_bgr() {
+	std::string bgr_str = 
+		"(B: " + std::to_string(bgr_val[0]) + 
+		", " + 
+		"G: " + std::to_string(bgr_val[1]) + 
+		", " + 
+		"R: " + std::to_string(bgr_val[2]) + 
+		")";
+	
+	return bgr_str;
 }
