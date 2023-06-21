272b472b731fc7d7b7f1c281366cc8cd413a1171
Andrew McLaughlin <andrewmcl6081@gmail.com>
2023-06-21 11:55:47 -0500

    added tool to reset image to original state

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 struct dataStruct {
 	cv::Mat* image;
-	bool needsUpdate;
+	cv::Mat* image_clean;
 };
 
 
@@ -37,9 +37,9 @@ int main(int argc, char **argv)
     }
     else
     {
-		cv::Mat imageIn;
-		imageIn = cv::imread(argv[1], cv::IMREAD_COLOR);
-		
+		cv::Mat imageIn = cv::imread(argv[1], cv::IMREAD_COLOR);
+		cv::Mat imageIn_clean = imageIn.clone();
+
 		// check for file error
 		if(!imageIn.data)
 		{
@@ -48,13 +48,12 @@ int main(int argc, char **argv)
 		}
 
 		// Initialize our struct to manage image data
-		// dataStruct imgData;
-		// imgData.image = &imageIn;
-		// imgData.needsUpdate = false;
-
+		dataStruct imgData;
+		imgData.image = &imageIn;
+		imgData.image_clean = &imageIn_clean;
 
 		cv::imshow(windowName, imageIn);
-		cv::setMouseCallback(windowName, toolsEvent, &imageIn);
+		cv::setMouseCallback(windowName, toolsEvent, &imgData);
 		cv::waitKey();
 
     }
@@ -65,7 +64,8 @@ void toolsEvent(int event, int x, int y, int flags, void* param) {
 
 	//std::cout << "in func" << std::endl;
 
-	cv::Mat* imageIn_ptr = (cv::Mat*)param;
+	dataStruct* imgData = static_cast<dataStruct*>(param);
+	cv::Mat* image_ptr = imgData->image;
 
 	// Pointer to the struct created in main
 	// dataStruct* imgData = static_cast<dataStruct*>(param);
@@ -82,9 +82,9 @@ void toolsEvent(int event, int x, int y, int flags, void* param) {
 	}
 	else if (toolIndex == 0 && event == cv::EVENT_LBUTTONDOWN) {
 
-		bgr_val[0] = imageIn_ptr->at<cv::Vec3b>(y,x)[0];
-		bgr_val[1] = imageIn_ptr->at<cv::Vec3b>(y,x)[1];
-		bgr_val[2] = imageIn_ptr->at<cv::Vec3b>(y,x)[2];
+		bgr_val[0] = image_ptr->at<cv::Vec3b>(y,x)[0];
+		bgr_val[1] = image_ptr->at<cv::Vec3b>(y,x)[1];
+		bgr_val[2] = image_ptr->at<cv::Vec3b>(y,x)[2];
 
 		std::cout << print_bgr() << std::endl;
 	}
@@ -103,13 +103,13 @@ void toolsEvent(int event, int x, int y, int flags, void* param) {
 
 			//std::cout << "p1 : " << p1 << " " << "p2 : " << p2 << std::endl;
 			
-			cv::Mat imageROI = (*imageIn_ptr)(region);
+			cv::Mat imageROI = (*image_ptr)(region);
 
-			// imageIn_ptr holds the address of imageIn in the main func.
+			// image_ptr holds the address of imageIn in the main func.
 			// dereferncing this pointer variable takes us to the address of where
 			// imageIn lives and we are modifying it with the newly modified ROI image
-			*imageIn_ptr = imageROI;
-			cv::imshow(windowName, imageROI);
+			*image_ptr = imageROI;
+			cv::imshow(windowName, *image_ptr);
 
 			//works as well
 			// Grabbing ROI from original imageIn and updating imageIn with new ROI image
@@ -123,12 +123,12 @@ void toolsEvent(int event, int x, int y, int flags, void* param) {
 	else if (toolIndex == 2) {
 
 		if (flags == cv::EVENT_FLAG_LBUTTON) {
-			(*imageIn_ptr).at<cv::Vec3b>(y,x)[0] = bgr_val[0];
-			(*imageIn_ptr).at<cv::Vec3b>(y,x)[1] = bgr_val[1];
-			(*imageIn_ptr).at<cv::Vec3b>(y,x)[2] = bgr_val[2];
+			image_ptr->at<cv::Vec3b>(y,x)[0] = bgr_val[0];
+			image_ptr->at<cv::Vec3b>(y,x)[1] = bgr_val[1];
+			image_ptr->at<cv::Vec3b>(y,x)[2] = bgr_val[2];
 			//std::cout << imageIn_ptr->at<cv::Vec3b>(y,x) << std::endl;
 
-			cv::imshow(windowName, *imageIn_ptr);
+			cv::imshow(windowName, *image_ptr);
 		}
 	}
 	else if (toolIndex == 3) {
@@ -140,8 +140,17 @@ void toolsEvent(int event, int x, int y, int flags, void* param) {
 			cv::Point targetPoint(x,y);
 			cv::Scalar fillColor(bgr_val[0], bgr_val[1], bgr_val[2]);
 
-			cv::floodFill(*imageIn_ptr, targetPoint, fillColor);
-			cv::imshow(windowName, *imageIn_ptr);
+			cv::floodFill(*image_ptr, targetPoint, fillColor);
+			cv::imshow(windowName, *image_ptr);
+		}
+	}
+	else if (toolIndex == 4) {
+
+		if(event == cv::EVENT_LBUTTONDBLCLK) {
+			cv::Mat image_clean_temp = *(imgData->image_clean);
+			*image_ptr = image_clean_temp.clone();
+			
+			cv::imshow(windowName, *image_ptr);
 		}
 	}
 
